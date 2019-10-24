fdabcfb2a0d569ab74916a0c4aaa7e84361fdaf4
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2019-10-24 14:53:01 -0500

    adi

---
+++ CARPETA/1/numeros-string.js
@@ -0,0 +1,20 @@
+var numeroUno = 1; // camelCase
+var numero_dos = 2;
+var resultado = 0;
+//SUMA
+resultado = 1 + 2;
+// RESTAR
+resultado = 1 - 2;
+// MULTIP´LICACION
+resultado = 1 * 2;
+// DIVIDIR
+resultado = 1 / 2;
+// EXPONENCIAL
+resultado = 1 ^ 2;
+// RESTAR
+resultado = 1 - 2;
+// MODD
+resultado = 4 % 2; //0---> reSSIDO
+resultado = 4 % 3; //1-->RESIDUO
+resultado = 7 % 3; // 
+console.log("pera" + 2);
---
+++ CARPETA/1/numeros-string.ts
@@ -0,0 +1,21 @@
+const numeroUno = 1; // camelCase
+const numero_dos = 2;
+let resultado = 0;
+//SUMA
+resultado = 1 + 2;
+// RESTAR
+resultado = 1 - 2;
+// MULTIP´LICACION
+resultado = 1 * 2;
+// DIVIDIR
+resultado = 1 / 2;
+// EXPONENCIAL
+resultado = 1 ^ 2;
+// RESTAR
+resultado = 1-2;
+// MODD
+resultado = 4%2;//0---> reSSIDO
+resultado = 4 % 3;//1-->RESIDUO
+
+resultado = 7 % 3; // 
+console.log("pera" + 2);
