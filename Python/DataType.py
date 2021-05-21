002db0a370abe43f6fe4e4ffec45bd93e40c4641
Atari777 <55715664+Atari777@users.noreply.github.com>
2021-05-21 17:38:10 -0500

    python

---
+++ Python/DataType.py
@@ -0,0 +1,24 @@
+print(type("hello  world")) # enterate, asi consigues el timpo de dato.
+
+print(type(30)) # Entero
+print(type(30.5))# Float
+# booleans
+True
+False 
+# List .. puedo cambiar los datos
+[10, True, 3.5]
+
+# tuplas -- datos no cambian
+
+(1, True, 2.4)
+
+# lista vacia
+[]
+
+# Diccionarios --Dic
+
+{
+    "nombre": "Ariel",
+    "Edad": 23
+}
+
---
+++ Python/variables.py
@@ -0,0 +1,9 @@
+# Python identifica como 2 variables distintas al diferenciar entre mayudculas y minusculas
+aur, book = "variable 1", 2 
+
+print(aur , book) # se imprimen en la misma linea
+
+myStr ="Hello"
+print(dir(myStr)) # DIR muestra las operaciones posibles sobre ese tipo de dato. como upper 
+
+print(myStr.upper())
